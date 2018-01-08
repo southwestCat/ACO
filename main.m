3677a029ecf67f9650b44cb77e4e24ab4ec57553
XuZihan <xuzihan.boy@gmail.com>
2018-01-08 11:42:14 +0800

    ACO
    comment not save

--- main.m
+++ main.m
@@ -109,14 +109,6 @@ OutputPath(Shortest_Route,cityName);
 %% 绘图
 DrawPath(Shortest_Route,X);
 pause(1);
-% plot([citys(Shortest_Route,1);citys(Shortest_Route(1),1)],...
-%      [citys(Shortest_Route,2);citys(Shortest_Route(1),2)],'o-');
-% grid on
-% for i = 1:size(citys,1)
-%     text(citys(i,1),citys(i,2),['   ' num2str(i)]);
-% end
-% text(citys(Shortest_Route(1),1),citys(Shortest_Route(1),2),'       起点');
-% text(citys(Shortest_Route(end),1),citys(Shortest_Route(end),2),'       终点');
 xlabel('城市位置横坐标')
 ylabel('城市位置纵坐标')
 title(['蚁群算法优化路径(最短距离:' num2str(Shortest_Length) ')'])
